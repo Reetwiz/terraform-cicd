cd83379a707c72cd6d2b40d0106f4f0b3362063b
Reetwiz <ekantakuna001@gmail.com>
2025-07-14 01:37:11 +0000

    changedev

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "reetwiz-terraform-backend-final"
-    key          = "terraform/state"
-    region       = "us-east-1"
-    encrypt      = true
+    bucket  = "reetwiz-terraform-backend-final"
+    key     = "terraform/state"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
